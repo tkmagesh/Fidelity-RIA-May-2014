34b36c28fcf507546367ba57913bbc99353df662
tkmagesh <tkmagesh77@gmail.com>
2014-05-27 10:58:27 +0530

    JavaScript - Language Features

---
+++ Day-3/JS-Language/CalculatorTests.js
@@ -0,0 +1,14 @@
+$(function(){
+	test("Should be able to add two numbers",function(){
+		//arrange
+		var number1 = 10,
+			number2 = 20,
+			expectedResult = 30;
+
+		//act
+		var actualResult = add(number1,number2);
+
+		//assert
+		return actualResult === expectedResult;
+	});
+});
